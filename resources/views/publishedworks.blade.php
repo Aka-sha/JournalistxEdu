1fea29fac81196142a0d270bd7d9a2795f4c8165
Aka'sh Carver <alcarve4@gmail.com>
2020-08-03 11:55:18 -0400

    Add files via upload

---
+++ resources/views/publishedworks.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>JournalistxEdu</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="page-container">
+    <div id="content-wrap">
+        <div id="header">
+            <img id="logo" src = "brand.png" alt="JournalistxEdu logo" width=120px>
+            <h1>JournalistxEdu</h1>
+        </div>
+
+        <div class = "menu-bar">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="students.html">Students</a>
+            <a href="journalists.html">Journalists</a>
+            <a class="active" href="publishedworks.html">Published Works</a>
+        </div>
+
+        <div id="apply">
+            <a class="button" href="apply.html">APPLY</a>
+        </div>
+        <div id="login">
+            <a class="button" href="login.html">LOGIN</a>
+        </div>
+        <div id="description">
+            <img id="image" src="published-works-image.jpg" alt="a typewriter" width=300>
+            <p id="text">Here is where we showcase some of our student pieces for all to see! We also provide links to view <br>the published work of our mentors.<br>
+                No published work yet!</p>
+        </div>
+        <div>
+            <footer id="footer">  </footer>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/students.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>JournalistxEdu</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="page-container">
+    <div id="content-wrap">
+        <div id="header">
+            <img id="logo" src = "brand.png" alt="JournalistxEdu logo" width=120px>
+            <h1>JournalistxEdu</h1>
+        </div>
+
+        <div class = "menu-bar">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a class="active" href="students.html">Students</a>
+            <a href="journalists.html">Journalists</a>
+            <a href="published-works.html">Published Works</a>
+        </div>
+
+        <div id="apply">
+            <a class="button" href="apply.html">APPLY</a>
+        </div>
+        <div id="login">
+            <a class="button" href="login.html">LOGIN</a>
+        </div>
+        <div id="description">
+            <img id="image" src="students-image.jpg" alt="a typewriter" width=300>
+            <p id="text">Students from all over the country are invited to apply to the JournalistxEdu program!
+                Accepted <br>participants are matched with a professional mentor whom they will spend one-on-one time <br>with to receive personalized feedback on their writing. There will also be opportunities <br>for group discussion and peer review, allowing students to communally synthesize information <br>and listen to diverse perspectives.
+                Students are expected to commit one and a half <br>hours per week to face-to-face interaction, in addition to time spent working on weekly <br>writing assignments. </div>
+    </div>
+    <div>
+        <footer id="footer"> </footer>
+    </div>
+</div>
+</body>
+</html>
